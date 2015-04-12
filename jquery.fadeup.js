9ccbde9a33af845ec6d3b62da330e67d0112027b
Matti Salokangas <matti.salokangas@gmail.com>
2015-04-11 22:18:50 -0700

    Update library with velocity

--- jquery.fadeup.js
+++ jquery.fadeup.js
@@ -11,41 +11,47 @@
 
 (function($) {
   $.fn.fadeUp = function (options) {
-
-    var settings = $.extend({},
-      $.fn.fadeUp.defaults,
-      options);
+    var settings = $.extend({}, $.fn.fadeUp.defaults, options);
 
     return this.each(function() {
-      var $el = $(this);
 
-      if ($el.height() === 0) {
-        $el.animate({
+      var $this = $(this),
+          $data = $this.data('fadeUp') || {};
+
+      if ($data.collapsed) {
+        $.fn.fadeUp.animate.call($this, {
+          borderTopWidth: $data.borderTopWidth,
+          borderBottomWidth: $data.borderBottomWidth,
           opacity: 1,
-          height: $el.data('fadeUp-height'),
-          minHeight: $el.data('fadeUp-min-height'),
-          paddingTop: $el.data('fadeUp-padding-top'),
-          paddingBottom: $el.data('fadeUp-padding-bottom'),
-          marginTop: $el.data('fadeUp-margin-top'),
-          marginBottom: $el.data('fadeUp-margin-bottom'),
-          borderSize: $el.data('fadeUp-border-size'),
-          lineHeight: $el.data('fadeUp-line-height'),
-          overflow: $el.data('fadeUp-overflow')
-        }, settings.duration);
+          height: $data.height,
+          minHeight: $data.minHeight,
+          paddingTop: $data.paddingTop,
+          paddingBottom: $data.paddingBottom,
+          marginTop: $data.marginTop,
+          marginBottom: $data.marginBottom,
+          overflow: $data.overflow
+        }, settings);
+
+        $data.collapsed = false;
       }
       else {
-        $el.data({
-          'fadeUp-height': $el.outerHeight(),
-          'fadeUp-min-height': $el.css('min-height'),
-          'fadeUp-padding-top': $el.css('paddingTop'),
-          'fadeUp-padding-bottom': $el.css('paddingBottom'),
-          'fadeUp-margin-top': $el.css('marginTop'),
-          'fadeUp-margin-bottom': $el.css('marginBottom'),
-          'fadeUp-overflow': $el.css('overflow')
+        $this.data($.fn.fadeUp.dataRoot, {
+          borderTopWidth: $this.css('borderTopWidth'),
+          borderBottomWidth: $this.css('borderBottomWidth'),
+          display: $this.css('display'),
+          height: $this.outerHeight(),
+          minHeight: $this.css('minHeight'),
+          paddingTop: $this.css('paddingTop'),
+          paddingBottom: $this.css('paddingBottom'),
+          marginTop: $this.css('marginTop'),
+          marginBottom: $this.css('marginBottom'),
+          overflow: $this.css('overflow'),
+          collapsed: true
         });
-        $el
-        .css('overflow', 'hidden')
-        .animate({
+
+        $.fn.fadeUp.animate.call($this, {
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
           opacity: 0,
           height: 0,
           minHeight: 0,
@@ -53,14 +59,13 @@
           paddingBottom: 0,
           marginTop: 0,
           marginBottom: 0
-        }, settings.duration);
+        }, settings);
       }
     });
-
-  }
-
-  $.fn.fadeUp.defaults = {
-    duration: 700
   };
+
+  $.fn.fadeUp.animate = $.fn.velocity ? $.fn.velocity : $.fn.animate;
+  $.fn.fadeUp.dataRoot = 'fadeUp';
+  $.fn.fadeUp.defaults = { duration: 700 };
 
 })(jQuery);
