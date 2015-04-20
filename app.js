af32410390b4c97e9d09418aced408a90440bbe2
Matti Salokangas <matti.salokangas@gmail.com>
2015-04-20 07:12:08 -0700

    Add app js

---
+++ app.js
@@ -0,0 +1,11 @@
+{
+  "name": "fadeUp",
+  "description": "A simple, jQuery plugin for fancy hiding.",
+  "repository": "https://github.com/sturdynut/fadeUp",
+  "keywords": [
+    "fadeUp",
+    "fade",
+    "show",
+    "hide"
+  ]
+}
