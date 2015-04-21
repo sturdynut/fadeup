af234a42d51876753f18eec614d2327fabd00060
Matti Salokangas <matti.salokangas@gmail.com>
2015-04-20 19:27:47 -0700

    Remove deprication warngin for sendFile

--- demo/app.js
+++ demo/app.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.sendfile('./index.html');
+  response.sendFile(__dirname + '/index.html');
 });
 
 app.listen(app.get('port'), function() {
